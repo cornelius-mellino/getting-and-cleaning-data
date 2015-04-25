3e6245b2317fe658ddcad25775cde785daa90e8c
Cornelius Mellino <cornelius.mellino@sigma.co.id>
2015-04-25 22:48:31 +0700

    Committing coursera project

---
+++ README.txt
@@ -0,0 +1,63 @@
+Getting and Cleaning Data
+
+This is a repository of all data and code written for the Getting and Cleaning Data topic from Coursera course (Johns Hopkins University).
+
+Course Project
+
+Find all project-related materials in the UCI HAR Dataset directory
+
+Unzip the source (https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip) into a folder on your local drive, mine is E:\r-workspace\
+
+Put run_analysis.R into E:\r-workspace\UCI HAR Dataset\
+
+In RStudio, type: 
+
+setwd("E:\\r-workspace\\UCI HAR Dataset\\")
+source("run_analysis.R")
+
+To read the data, type: 
+data <- read.table("final_data_set_with_averages.txt") 
+
+Data dimension: 180x69 
+
+Subject: 30
+Activities: 6
+
+30x6 = 180 (rows)
+
+First 3 columns contains these information:
+1. Subject (numeric)
+2. Activity Id (numeric)
+3. Activity (character)
+
+Row 4-69 (66 columns) contains the mean of the mean and standard deviation of these measured variables:
+
+1. tBodyAcc-XYZ
+2. tGravityAcc-XYZ
+3. tBodyAccJerk-XYZ
+4. tBodyGyro-XYZ
+5. tBodyGyroJerk-XYZ
+6. tBodyAccMag
+7. tGravityAccMag
+8. tBodyAccJerkMag
+9. tBodyGyroMag
+10.tBodyGyroJerkMag
+11.fBodyAcc-XYZ
+12.fBodyAccJerk-XYZ
+13.fBodyGyro-XYZ
+14.fBodyAccMag
+15.fBodyAccJerkMag
+16.fBodyGyroMag
+17.fBodyGyroJerkMag
+
+All in numeric fraction ranged [-1 to +1]
+
+The dataset includes the following files:
+=========================================
+
+- 'README.txt'
+
+- 'README.md'
+
+- 'final_data_set_with_averages.txt' : Data set files that contains average of each measured variables for each Subject and Activity
+
---
+++ run_analysis.R
@@ -0,0 +1,75 @@
+# 1. Merges the training and the test sets to create one data set.
+
+# Load the experiment data
+temporaryTable1 <- read.table("train/X_train.txt")
+temporaryTable2 <- read.table("test/X_test.txt")
+X <- rbind(temporaryTable1, temporaryTable2)
+
+# Load the subject data
+temporaryTable1 <- read.table("train/subject_train.txt")
+temporaryTable2 <- read.table("test/subject_test.txt")
+S <- rbind(temporaryTable1, temporaryTable2)
+
+# Load the activity
+temporaryTable1 <- read.table("train/y_train.txt")
+temporaryTable2 <- read.table("test/y_test.txt")
+Y <- rbind(temporaryTable1, temporaryTable2)
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+
+# Load the features
+features <- read.table("features.txt")
+# Extract variables that related to mean and std
+varsIndex <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
+X <- X[, varsIndex]
+names(X) <- features[varsIndex, 2]
+# Remove the ()
+names(X) <- gsub("\\(|\\)", "", names(X))
+
+# 3. Uses descriptive activity names to name the activities in the data set.
+
+# Load activities
+activityLabels <- read.table("activity_labels.txt")
+# Remove the _ character
+activityLabels[, 2] = gsub("_", " ", activityLabels[, 2])
+# Bind the column of activity names but keep the activity id 
+Y <- cbind(Y, Y[,1])
+Y[,2] = activityLabels[Y[,1], 2]
+names(Y) <- c("ActivityId", "Activity")
+
+# 4. Appropriately labels the data set with descriptive activity names.
+
+# Combine the tables and save to final_data_set.txt file
+names(S) <- "Subject"
+dataSet <- cbind(S, Y, X)
+write.table(dataSet, "final_data_set.txt")
+
+# 5. Creates a 2nd, independent tidy data set with the average of each variable for each activity and each subject.
+
+# Setup the necessary parameters to run the process
+uniqueSubjects = unique(S)[,1]
+numSubjects = length(uniqueSubjects)
+numActivities = length(activityLabels[,1])
+numCols = dim(dataSet)[2]
+result = dataSet[1:(numSubjects*numActivities), ]
+
+# Start from the first row
+row = 1
+for (s in 1:numSubjects) {
+  for (a in 1:numActivities) {
+    # Add subject at the 1st column
+    result[row, 1] = uniqueSubjects[s]
+    # Add activity id at the 2nd column
+    result[row, 2] = a
+    # Add activity name at the 3rd column
+    result[row, 3] = activityLabels[a, 2]
+    # Find the average of each column for each s and a
+    tmp <- sapply(dataSet[(dataSet$Subject==s & dataSet$ActivityId==a),4:ncol(dataSet)], mean)
+    # Add the average result row from column 4 to the last column
+    result[row, 4:numCols] <- tmp[1:length(tmp)]
+    row = row+1
+  }
+}
+
+# Write the result to final_data_set_with_averages.txt file
+write.table(result, "final_data_set_with_averages.txt", row.name=FALSE)
